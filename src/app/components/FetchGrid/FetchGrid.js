41d0cd63bded11468772068a6bcc26dc4d15fc5f
augusto lupo <augustolupo030422@gmail.com>
2024-05-16 16:23:16 -0300

    Ultima Actualizacion

--- src/app/components/FetchGrid/FetchGrid.js
+++ src/app/components/FetchGrid/FetchGrid.js
@@ -1,26 +1,40 @@
-'use client'
+"use client";
 
-import { useEffect } from "react"
-import axios from "axios"
+import { useEffect, useState } from "react";
+import axios from "axios";
 import styles from "./FetchGrid.module.css"
 
 const FetchGrid = () => {
-    useEffect(() => {
-        const handleFetchData = async () => {
-            const response = await axios.get ("https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=The+Strokes&api_key=d659cc02b91f8569a8d3a3e95091db43&format=json")
-            
-            const data = response.data.topalbums.album;
-            console.log(data)
-        }
+  const [albums, setAlbums] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-        handleFetchData()
-    }, [])
+  useEffect(() => {
+    setLoading(true);
+    const handleFetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=The+Strokes&api_key=d659cc02b91f8569a8d3a3e95091db43&format=json"
+        );
+        const data = response.data.topalbums.album;
+        setAlbums(data);
+        setLoading(false);
+        console.log("data", data);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
 
-    return (
-        <div className={styles["container"]}>
-            <h1 className={styles["title"]}>Fetching data</h1>
-        </div>
-    )
-}
+    handleFetchData();
+  }, []);
 
-export default FetchGrid
+  return (
+    <div className={styles["Fetching"]}>
+    <p className={styles["title"]}>Fetching data</p>
+      {loading && <p>Fetching data</p>}
+      {!loading &&
+        albums.map((album, index) => <p className={styles["albums-style"]} key={index}>{album.name}</p>)}
+    </div>
+  );
+};
+
+export default FetchGrid;
